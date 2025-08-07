002831e96a0f9af943d3cf928f7a5bc43e1aaa02
angel <AngelJimS@outlook.com>
2025-08-07 12:47:05 +0200

    feat: add new content view

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,17 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emoji: Array<String> = ["👻", "🎃", "🕷️", "😈"]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView(content: "👻", isFaceUp: false)
+            CardView(content: "🎃")
+            CardView(content: "🕷️", isFaceUp: true)
+            CardView(content: "😈", isFaceUp: true)
         }
         .padding()
+        .foregroundColor(.orange)
     }
 }
 
-#Preview {
-    ContentView()
+struct CardView: View {
+    let content: String
+    @State var isFaceUp = false
+
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            if isFaceUp {
+                base.fill(Color.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+            } else {
+                base.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+            
+        }
+    }
 }
+                
+
+            
